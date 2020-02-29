0bfb4ca2e111d0cb4e33e6667c16fe4505e23a40
Andy Turner <turner.andrew.1990@gmail.com>
2020-02-29 12:48:27 +0000

    populated home.vue with text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
-    Home
+     <v-card>
+      <v-card-title>
+        <h2>Github User Search</h2>
+      </v-card-title>
+      <v-card-text>
+        <p>Welcome to the GitHub user search app. This is a simple web application for search the activity and repositories of GitHub users.</p>
+        <p>To use the app type the name of the user you wish to query. This will present a list of current GitHub users which match your search. You then click on the user you wish to view, or type an alternative search.</p>
+        
+      </v-card-text>
+     </v-card>
   </div>
 </template>
 
