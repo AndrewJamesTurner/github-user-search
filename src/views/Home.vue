a21ee9c618b593bd1a430a73f9b0793edcc3c142
Andy Turner <turner.andrew.1990@gmail.com>
2020-02-29 17:52:45 +0000

    updated home page text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,8 @@
         <h2>Github User Search</h2>
       </v-card-title>
       <v-card-text>
-        <p>Welcome to the GitHub user search app. This is a simple web application for search the activity and repositories of GitHub users.</p>
-        <p>To use the app type the name of the GitHub user you wish to search for in the top bar. This will present a list of GitHub users which match your search. Then click on the GitHub user you wish to view, or type an alternative search.</p>
-        
+        <p>Welcome to the GitHub user search app. This is a simple web application for searching GitHub users and viewing their profile, repositories and recent activity.</p>
+        <p>To use the app type the name of the GitHub user you wish to search for in the top bar. This will present a list of GitHub users who match your search. Then click on the GitHub user you wish to view, or enter an alternative search.</p>
       </v-card-text>
      </v-card>
   </div>
