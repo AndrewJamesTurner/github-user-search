dc9a22a167e307894c8ed0e9877563b1dde01e3e
Andy Turner <turner.andrew.1990@gmail.com>
2020-02-29 18:05:55 +0000

    added comments in helper.ts

--- src/ts/helper.ts
+++ src/ts/helper.ts
@@ -1,5 +1,8 @@
 import moment from 'moment'
 
+// converts a given date string i.e. 
+// 01 January, 1970 00:00:00 Universal Time (UTC)
+// into a string representing just the date i.e. 01-01-1970
 export const formatDate = (dateString: string) => {
 
     const date = new Date(dateString);
